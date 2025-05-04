fd19d727bcb99bb1f9c2d34a9fc8ba0e7690df69
juhani.kuru <juhanialeksikuru@gmail.com>
2025-05-04 18:43:12 +0300

    add cors

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from controllers.blogs import router as blogs_router
 
 app = FastAPI()
+
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True, allow_methods=['*'],
+                   allow_headers=['*'])
 
 app.include_router(blogs_router)
 
